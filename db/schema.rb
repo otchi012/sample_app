c111e23b19f971d84bf0a88f722e31fc4b57f215
otchi012 <hiroshi012931@gmail.com>
2020-10-06 06:30:36 +0000

    [Finish]2-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_052712) do
+ActiveRecord::Schema.define(version: 2020_10_06_061955) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
