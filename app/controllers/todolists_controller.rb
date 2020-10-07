09e5ee1e7f3fd9f509d2429705ea0a78937df3dd
otchi012 <hiroshi012931@gmail.com>
2020-10-07 10:41:15 +0000

    [Finish]2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -19,6 +19,18 @@ class TodolistsController < ApplicationController
 
   end
 
+  def edit
+    @list = List.find(params[:id])
+
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path
+
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
