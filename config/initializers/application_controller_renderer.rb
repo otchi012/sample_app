684a3b4d44b60f87089f24cb8248e4f956088f4e
otchi012 <hiroshi012931@gmail.com>
2020-10-07 12:28:29 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b59e6699c226666b7087f635579583693e3be763b9f4aa8e349d19aaabb92ecce198552ea1a7a32f558041e7aa26ccabc6cc70f49588ae444cd51aed65edaaa2'
